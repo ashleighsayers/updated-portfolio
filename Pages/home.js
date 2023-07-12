9b7eadf291c556fb965a2cc134787c657d449a6e
Ashleigh Sayers <ashliegh.sayers01@outlook.com>
2023-07-12 14:44:19 +0800

    nav bar

--- Pages/home.js
+++ Pages/home.js
@@ -2,6 +2,8 @@ console.log("home js working")
 
 let root = document.querySelector("#root");
 
+
+
 const home = `
     <nav>
         <div class="nav"> 
@@ -11,11 +13,23 @@ const home = `
             
             <div class="nav-content"> </div>
             <div class="nav-content nav-links">
-                <ul>
-                    <li>Resume</li>
-                </ul>
+                
+                <button class="hamburger hamburger--minus " type="button">
+                <span class="hamburger-box">
+                  <span class="hamburger-inner">
+                
+                  </span>
+                </span>
+              </button>
+                
             </div>
+
+            <div class="nav-inner">
+
         </div>
+        </div>
+
+        
     </nav>
 
     <main>
@@ -29,3 +43,12 @@ const home = `
 `;
 
 document.body.innerHTML = home;
+
+let nav = document.querySelector('.hamburger')
+
+nav.addEventListener("click", ()=> {
+    nav.classList.toggle("is-active")
+
+    let navInner = document.querySelector(".nav-inner")
+    navInner.classList.toggle("nav-open")
+})
